e03450746ef5dd52f3b338c6cf1cc86b7207d475
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-09-05 08:18:03 -0700

    add firebase configure

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,21 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+// firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeiZ-838gDATVqV5ykyCpevaWXkBMJqaM",
-  authDomain: "task-manager-ead57.firebaseapp.com",
-  projectId: "task-manager-ead57",
-  storageBucket: "task-manager-ead57.firebasestorage.app",
-  messagingSenderId: "544806961634",
-  appId: "1:544806961634:web:a8ec18ef16a718c84579bb"
-}
+  apiKey: "AIzaSyA0w4auw0CSlnbjQkuyYML1gNIvNCq00e4",
+  authDomain: "foundlyapp.firebaseapp.com",
+  projectId: "foundlyapp",
+  storageBucket: "foundlyapp.firebasestorage.app",
+  messagingSenderId: "157719197896",
+  appId: "1:157719197896:web:07ce2be8a56bfd27d10dfd",
+  measurementId: "G-6YG34W3NBM"
+};
 
-const app = initializeApp(firebaseConfig)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+// Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
