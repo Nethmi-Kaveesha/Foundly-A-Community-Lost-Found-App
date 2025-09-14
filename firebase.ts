27694c36a133526635ca77af393766e0edae9e97
Nethmi Kaveesha <141547746+Nethmi-Kaveesha@users.noreply.github.com>
2025-09-14 03:29:58 -0700

    add storage

--- firebase.ts
+++ firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0w4auw0CSlnbjQkuyYML1gNIvNCq00e4",
@@ -19,3 +20,4 @@ const app = initializeApp(firebaseConfig);
 // Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
